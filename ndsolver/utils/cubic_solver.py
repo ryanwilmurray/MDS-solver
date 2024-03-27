efb4c2af240dbd2f2855d57b49ff4c7704d597e4
bjtroy <bjtroy@ncsu.edu>
2024-03-26 22:17:46 -0400

    Added parametric solver

--- ndsolver/utils/cubic_solver.py
+++ ndsolver/utils/cubic_solver.py
@@ -38,3 +38,54 @@ def vectorized_depressed_cubic_roots(P: np.ndarray, Q: np.ndarray) -> np.ndarray
     r3 = (-(v1+v2)/2-(v1-v2)*cmath.sqrt(3)/2j).real*M
     r = np.column_stack((r1,r2,r3))
     return r
+
+
+
+def parametric_root_finder_1d(X: np.ndarray, a, b, c) -> np.ndarray:
+    """_summary_
+
+    Args:
+        X (np.ndarray): (d, n) data matrix
+        a (np.ndarray): (d,) vector of parameters
+        b (float): parameter
+        c (float): parameter
+        
+    Returns:
+        np.ndarray: (n, 3) array of roots
+    """
+    # TODO: this step can be cached
+    X_norms_squared = LA.norm(X, axis=0)**2
+    M2 = np.mean(X_norms_squared)
+    
+    p = -X_norms_squared - M2 + b
+    q = a.T @ X + c
+    
+    D = q**2/4+p**3/27
+    
+    # Removes need for 'if' statement
+    # D[i]>0 -> M[i]=0; D[i]<0 -> M[i]=1
+    M = (1-np.sign(D))/2
+    
+    # same as before
+    u1 = -q/2 + np.emath.sqrt(D)
+    u2 = -q/2 - np.emath.sqrt(D)
+    v1 = np.emath.power(u1, 1/3)
+    v2 = np.emath.power(u2, 1/3)
+    r1 = (v1+v2).real
+    
+    # r2 & r3 will be non existent when D>0
+    r2 = (-(v1+v2)/2+(v1-v2)*cmath.sqrt(3)/2j).real*M
+    r3 = (-(v1+v2)/2-(v1-v2)*cmath.sqrt(3)/2j).real*M
+    r = np.column_stack((r1,r2,r3))
+    
+    Z = r ** 2 * -np.expand_dims(p, axis=1) + 3*r*-np.expand_dims(q, axis=1)
+    
+    return r[np.arange(np.shape(p)[0]), np.argmax(Z, axis=1)]
+    
+    
+X = np.random.randn(10, 100)
+a = np.random.randn(10)
+b = 1
+c = 1
+r = parametric_root_finder_1d(X, a, b, c)
+print(r.shape)
---
+++ ndsolver/versions/parametric.py
@@ -0,0 +1,220 @@
+#Normalzed the cost, restructed so that the moment matricies are attributes so to easily plot. Added functionality to swap between the if statements
+# in the notebook. Added the updated critical lengths function to account for several roots. Changed visualize to just show phi and Y. Experimented
+# with renormalizing Y at each step.
+
+import matplotlib.pyplot as plt
+import numpy as np
+from numpy import linalg as LA
+from matplotlib.colors import ListedColormap, BoundaryNorm
+import scipy.linalg as spla
+from ndsolver.utils.cubic_solver import parametric_root_finder_1d
+
+class MM:
+    """Class for testing marginal minimization using the quartic descent algorithm. ONLY WORKS IN 1D FOR NOW.
+    """
+    
+    def __init__(self, X: np.ndarray, labels: list, m=2, init: np.ndarray = None, init_scaling_factor=1.25):
+        """Initializes a MM object for performing marginal minimiztion with the specified parameters.
+
+        Args:
+            X (np.ndarray): (d, n) the input data points
+            labels (np.array): (n,) the labels for the input data points
+            m (int, optional): The embedding dimensionality. Defaults to 2.
+            init (np.ndarray, optional): The initial embedding to improve. Random normal distribution if None. Defaults to None.
+            init_scaling_factor (float, optional): The scaling factor of the random normal distribution. Defaults to 0.1.
+        """
+        self.m = m
+        self.labels = labels
+        
+        # format the shape of the data X correctly 
+        a, b = X.shape
+        if a>b:
+            X = X.T
+        else:
+            X = X
+        self.d, self.n = X.shape
+
+        # normalize the data; otherwise gradient is completely unstable
+        # due to the lines above, these are always the correct axis
+        X-=np.mean(X,axis=1, keepdims=True)
+        X/=np.std(X,axis=1, keepdims = True)
+        self.X = X
+        
+        # center the data in Y; otherwise nothing is valid
+        Y = np.random.randn(m, self.n) * init_scaling_factor if init is None else init
+        self.Y = Y - np.mean(Y, axis=1, keepdims=True)
+        #self. Y /= np.std(self.Y,axis=1, keepdims = True)
+        
+        self.a, self.b, self.c = self.initialize_parameters()
+        self.cost = self.get_cost()
+        
+        
+    def get_cost(self,Y=None) -> float:
+        if Y is None:
+            Y=self.Y
+        dissim = (self.X.T * self.X.T) @ np.ones((self.d, self.n)) - 2 * self.X.T @ self.X + np.ones((self.n, self.d)) @ (self.X * self.X) - (
+                       (Y.T * Y.T) @ np.ones((self.m, self.n)) - 2 * Y.T @ Y + np.ones((self.n, self.m)) @ (Y * Y)).astype('float64')
+        c = ((np.tensordot(dissim, dissim) / 2)/(self.n**2))**(1/4)
+        return c
+   
+   
+    def initialize_parameters(self):
+        """Finds the moment matrices associated with the marginal problem at the current state in Y.
+        """
+
+        # TODO some parts of the computation below are not changing at all and should not be computed each time -  rather should be stored in init
+        norm_x_squared = spla.norm(self.X, axis=0)**2
+        norm_y_squared = spla.norm(self.Y, axis=0)**2
+
+        a = 2 / self.n * self.Y @ self.X.T
+        b = 3 * np.mean(norm_y_squared)
+        c = np.mean(self.Y * (norm_y_squared - norm_x_squared).reshape(self.n, 1, 1))
+        
+        return a.flatten(), b, c
+    
+    
+    def grad(self) -> np.ndarray:
+        Y = parametric_root_finder_1d(self.X, self.a, self.b, self.c)
+        X_norms_squared = LA.norm(self.X, axis=0)**2
+        M2 = np.mean(X_norms_squared)
+        
+        return np.sum(
+            ((LA.norm(self.X[:,i] - self.X[:,j])**2 - (Y[i] - Y[j])**2) * (Y[i] - Y[j])) * np.array([*self.X[:,i], -Y[i], -1]) /
+            (3 * Y[i]**2 - LA.norm(self.X[:,i])**2 - M2 - self.b)
+            for j in range(self.n) for i in range(self.n)
+        )
+    
+    
+    def update_state(self, step_size = 100, iterations=1000):
+        Y = self.quartic_descent_vectorized(self.psi, self.phi.T, step_size, iterations).T
+        self.Y = Y-np.mean(Y, axis=1, keepdims=True)
+        #self. Y /= np.std(self.Y,axis=1, keepdims = True)
+        self.psi, self.phi = self.initialize_parameters() 
+        self.cost = self.get_cost()
+        
+
+    def quartic_initialization_vectorized(self, A: np.ndarray, b: np.ndarray):
+        """
+        Creates vectorized initialization for the quartic descent algorithm.
+
+        Args:
+            A (np.ndarray): (n, m, m) the vectorized quadratic coefficients of the quartic functions (psi)
+            b (np.ndarray): (n, m) the vectorized linear coefficents of the quartic functions (phi)
+
+        Returns:
+            np.ndarray: (n, m) matrix of initial values for the quartic descent algorithm.
+        """
+        # Get broadcasted vector of eigenvalues (n x m) and matrix of eigenvectors (n x m x m)
+        v, V = LA.eig(A)
+
+        isotropic_guys = np.abs(v[:,0]-v[:,1]) < 1
+
+
+        k = np.einsum('ikj,ik->ij', V, b)
+        lengths = vectorized_depressed_cubic_roots(v,k)
+            
+            
+        mag_k = np.expand_dims(LA.norm(k, axis = 1), axis = 1)
+        I1 = (np.sum(v, axis=1,keepdims = True) / v.shape[1])*lengths**2+3*mag_k*lengths
+        indices_ = np.argmax(I1, axis=1)
+        opt_lengths = lengths[np.arange(self.n),indices_]
+        unit_phi = k/mag_k
+        unit_phi *= np.expand_dims(opt_lengths, axis = 1)
+        y0_ = np.einsum('ijk,ik->ij', V, unit_phi)
+            
+
+        happy_points = np.all(v<0,axis=1)
+        v = np.maximum(v, 0)
+        for i in range(k.shape[0]):
+            for j in range(k.shape[1]):
+                if k[i, j] == 0:
+                    k[i, j] = np.random.choice([-1, 1])
+
+
+        wells = v**2 + 4*np.abs(k) * np.sqrt(v)
+        indices = np.argmax(wells, axis=1)
+        z0 = np.zeros(b.shape)
+        z0[np.arange(b.shape[0]), indices] = 1
+        z0 *= np.sqrt(v) * -np.sign(k)
+
+
+        y0 = np.einsum('ijk,ik->ij', V, z0)
+        y0[happy_points] = 0
+        y0[isotropic_guys] = y0_[isotropic_guys]
+
+        return y0
+
+
+    def quartic_descent_vectorized(self, A: np.ndarray, b: np.ndarray, step_size=100, iterations=1000):
+        """Performs a gradient descent on a vectorized quartic function.
+
+        Args:
+            A (np.ndarray): (n, m, m) the vectorized quadratic coefficients of the quartic functions (psi)
+            b (np.ndarray): (n, m) the vectorized linear coefficents of the quartic functions (phi)
+            step_size (float, optional): The relative distance to move by each iteration. Defaults to 0.1.
+            iterations (int, optional): The number of iterations to perform. Defaults to 100.
+
+        Returns:
+            _type_: _description_
+        """
+        y = self.quartic_initialization_vectorized(A, b)
+
+        for _ in range(iterations):
+            gradient = 4 * (LA.norm(y, axis=1).reshape(b.shape[0], 1)**(3) * y - np.einsum('ijk,ik->ij', A, y) + b)
+            y -= step_size * gradient / (np.abs(gradient)**1 + 0.001)
+            #y -= step_size * gradient 
+        return y
+    
+
+    def visualize(self):
+        current_cost = self.cost
+        formatted_cost = f"{current_cost:.4}"
+        unique_labels = np.unique(self.labels)
+        colormap = ListedColormap(plt.cm.gist_rainbow(np.linspace(0, 1, len(unique_labels))))
+        norm = BoundaryNorm(np.arange(len(unique_labels)+1)-0.5, len(unique_labels))
+
+        plt.figure(figsize=(18, 5))  # Adjusted figure size to accommodate 3 plots
+
+        # First plot: Visualization based on labels
+        plt.subplot(1, 3, 1)
+        plt.scatter(self.Y[0, :], self.Y[1, :], c=self.labels, cmap=colormap, norm=norm, alpha=0.75)
+        cbar = plt.colorbar(ticks=np.arange(len(unique_labels)))
+        cbar.set_ticklabels(unique_labels)
+        cbar.set_label('Labels')
+        plt.title(f'qMDS Cost = {formatted_cost}')
+        
+        plt.subplot(1, 3, 2)
+        plt.scatter(self.phi[0, :], self.phi[1, :], c=self.labels, cmap=colormap, norm=norm, alpha=0.75)
+        cbar = plt.colorbar(ticks=np.arange(len(unique_labels)))
+        cbar.set_ticklabels(unique_labels)
+        cbar.set_label('Labels')
+        plt.title(f'Phi')
+        
+        Z = self.quartic_initialization_vectorized(self.psi, self.phi.T).T
+        init_cost = self.get_cost(Z)
+        formatted_init_cost = f"{init_cost:.4}"
+        plt.subplot(1, 3, 3)
+        plt.scatter(Z[0, :], Z[1, :], c=self.labels, cmap=colormap, norm=norm, alpha=0.75,clip_on = False)
+        cbar = plt.colorbar(ticks=np.arange(len(unique_labels)))
+        cbar.set_ticklabels(unique_labels)
+        cbar.set_label('Labels')
+        plt.title(f'Y0 cost={formatted_init_cost}')
+        
+        plt.tight_layout()
+        #plt.show()
+        
+        
+    def critical_lengths(self) -> np.ndarray:
+        """Calculates the critical lengths of the cubic equation based on 
+        the eigenvalues and eigenvectors of the moment matrix psi.
+
+        Returns:
+            np.ndarray: roots of the cubic equation
+        """
+        #A, b = self.find_moment_matrices(self.X, self.Y)
+        v, V = LA.eig(self.psi)
+        k = np.einsum('ikj,ik->ij', V, self.phi.T)
+        return vectorized_depressed_cubic_roots(v, k)
+  
+        
+        
